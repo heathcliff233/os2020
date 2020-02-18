b2d0aeaf89fa1f743b7dd8d10346b3714b0061b4
Bright Hong <brighthong233@gmail.com>
2020-02-18 21:14:12 +0800

    pass hard test, clean up code, finish pstree

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -14,22 +14,10 @@ const char* MAX_PID_FILE = "/proc/sys/kernel/pid_max";
 int arg_n = 0;
 int arg_p = 0;
 
-/*
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]);
-  return 0;
-}
-*/
-
-
 typedef struct Process{
     char *comm; //The filename of the executable, in parentheses.
-    int ppid; //The PID of the parent of the process.
-    int child;    //The child of the process.
+    int ppid;   //The PID of the parent of the process.
+    int child;  //The child of the process.
     int next;   //The brother of the process.
     int is_root;//Whether it should be expanded, determine the style to print.
 } Process;
@@ -37,18 +25,10 @@ typedef struct Process{
 Process* proc;  //Table of process information
 pid_t pid_max;  //Index of the max PID
 
-char tabs[1024];
-int tabs_ptr = 0;
-//int* list;
-
 void init();
 void scan_root();
 void scan_task();
-int is_digit(char* s);
 void add_child(int parent, int child);
-//void tree_sort(int root);
-//int tree_cmp(int p1, int p2);
-//void print_tree(int root);
 void printProcess(int r);
 void printParentProcess(int r);
 
@@ -57,7 +37,6 @@ int main(int argc, char *argv[]){
     // To preprocess args
     for (int i = 0; i < argc; i++){
         assert(argv[i]); // always true
-        // printf("argv[%d] = %s\n", i, argv[i]);
         if (argv[i][0] == '-'){
             if (argv[i][1] == '-'){
                 if (strcmp(argv[i], "--numeric-sort") == 0){
@@ -82,19 +61,12 @@ int main(int argc, char *argv[]){
             }
         }
     }
-
     assert(!argv[argc]); // always true
-
-    // printf("n: %d, p: %d\n", arg_n, arg_p);
 
     init();
     scan_root();
-    memset(tabs, 0, sizeof(tabs_ptr));  //TODO: fix it! Segmentation fault
-    //print_tree(1);
-    printf("start print\n");
     printProcess(1);
 
-    //free(list);
     free(proc);
     return 0;
 }
@@ -107,9 +79,6 @@ void init(){
     fscanf(pFile, "%d", &pid_max); // Get the max pid
     fclose(pFile);
 
-    //list = (int *)malloc(sizeof(int) * ((int)pid_max + 1));
-    //memset(list, 0, sizeof(int) * ((int)pid_max + 1));
-
     proc = (Process *)malloc(sizeof(Process) * (pid_max + 1));
     for (int i = 0; i < pid_max; i++){
         proc[i].comm = NULL;
@@ -119,15 +88,6 @@ void init(){
         proc[i].is_root = 0;
     }
 }
-
-int is_digit(char* s){
-    //Use library function to improve robustness
-    while(isdigit(*s-'0')){
-        ++s;
-    }
-    return *s=='\0';
-}
-
 
 
 void scan_root(){
@@ -140,12 +100,9 @@ void scan_root(){
         if((p->d_name[0] < '0')||(p->d_name[0] > '9')){
             continue;
         }
-        //printf("in loop\n");
 
-        //char tmp[256];
         char proc_dir[256];
         char proc_stat[256];
-        //snprintf(tmp, 256, "%s%s", ROOT_DIR, "/");
         snprintf(proc_dir, 256, "%s%s", ROOT_DIR, p->d_name); //Path of the directory
         snprintf(proc_stat, 256, "%s%s", proc_dir, "/stat");         //Path of the stat file
 
@@ -157,15 +114,12 @@ void scan_root(){
         //Read the stat file to pid structure table
         FILE* st;
         st = fopen(proc_stat, "r");
-        //printf("%s\n",proc_stat);
         if(st == NULL){
             continue;
         }
         fscanf(st, "%d (%[^)]) %s %d", &pid, comm, state, &ppid); 
         // %[^)] is to match to the char before ')'
 
-        //if(pid==1)printf("found pid 1\n");
-        //printf("PID %d\n",pid);
         fclose(st);
         // TODO: deal with special cases of PID 0, 1, 2
        /* 
@@ -173,7 +127,7 @@ void scan_root(){
         * PID0 is the ppid of PID2
         * PID2 is the ppid of all system calls
         */
-        if( /*ppid==0 ||*/ ppid==2 || !(ppid<pid_max && pid<pid_max) ){
+        if( ppid==2 || !(ppid<pid_max && pid<pid_max) ){
             continue;
         }
         int comm_len = strlen(comm) + 1;
@@ -280,7 +234,6 @@ void add_child(int ppid, int pid){
 
 void printProcess(int p){//proc) {
   /* print (pid) to name */
-
   printf("%s%s%s", 
       (p==1 ? "":(p == proc[proc[p].ppid].child ? (proc[p].next!=0 ? "-+-" : "---") : (proc[p].next ? " |-" : " `-"))), 
       proc[p].comm, 
@@ -301,71 +254,3 @@ void printParentProcess(int p) {
       (int) strlen(proc[p].comm), "");
 }
 
-/*
-void print_tree(int r){
-    //Print the proc structure
-    if (r == 0){
-        return;
-    }
-
-    int tabs_ptr_backup = tabs_ptr;
-
-    if (r != 1){
-        printf("-");
-        tabs_ptr += 1;
-    }
-    if (proc[r].is_root == 0){ 
-        // have no child, print within {}
-        printf("{%s}", proc[r].comm);
-        tabs_ptr += (strlen(proc[r].comm) + 2);
-    }else{
-        printf("%s", proc[r].comm);
-        tabs_ptr += strlen(proc[r].comm);
-    }
-    if (arg_p == 1){
-        printf("(%d)", r);
-        char pid_str[32];
-        sprintf(pid_str, "%d", r);
-        tabs_ptr += (2 + strlen(pid_str));
-    }
-    if (proc[r].child != 0){
-        if (proc[proc[r].child].next != 0){
-            tabs[tabs_ptr + 1] = 1;
-            printf("-+");
-        }else{
-            tabs[tabs_ptr + 1] = 0;
-            printf("--");
-        }
-        tabs_ptr += 2;
-        // tabs[tabs_ptr - 1] = 1;
-        print_tree(proc[r].child);
-        int son_ptr = proc[proc[r].child].next;
-        while (son_ptr != 0){
-            for (int i = 0; i < tabs_ptr - 1; i++){
-                if (tabs[i] == 0){
-                    printf(" ");
-                }
-                else{
-                    printf("|");
-                }
-            }
-            if (proc[son_ptr].next != 0){
-                printf("|");
-            }else{
-                tabs[tabs_ptr - 1] = 0;
-                printf("`");
-            }
-            print_tree(son_ptr);
-            son_ptr = proc[son_ptr].next;
-        }
-    }
-    else{
-        printf("\n");
-    }
-    for (int i = tabs_ptr_backup; i < tabs_ptr; i++){
-        tabs[i] = 0;
-    }
-    tabs_ptr = tabs_ptr_backup;
-    return;
-}
-*/
