90cbe4c2ba4a24100756b9b331f663da16e6a0f3
Bright Hong <brighthong233@gmail.com>
2020-02-22 19:46:10 +0800

    more attempts

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -1,7 +1,14 @@
 #include <game.h>
 #include <klib.h>
 
-#define FPS 1
+#define FPS 10
+#define W 0xffffff
+#define B 0x000000
+#define R 0xff0000
+#define ISKEYDOWN(x) (((x)&0x8000))
+#define KEYCODE(x) ((x)&0x7fff)
+
+uint32_t orange[1000];
 
 // Operating system is a C program!
 int main(const char *args) {
@@ -14,22 +21,31 @@ int main(const char *args) {
   splash();
 
   puts("Press any key to see its key code...\n");
-  //int next_frame = 0;
+  int next_frame = 0;
+  int is_halt = 0;
+  int is_print = 0;
   while (1) {
-    //while(uptime() < next_frame);
+    is_print = 0;
+    while(uptime() < next_frame);
     int key = _KEY_NONE;
-    splash();
-    while (key == _KEY_NONE) {
-      key = read_key();
+    for(int i=0;i<1000;++i) orange[i]=R;
+    //splash();
+    while (key=read_key != _KEY_NONE) {
+      if (KEYCODE(key)==_KEY_ESCAPE){
+        if(ISKEYDOWN(key)) is_halt = 1;
+      }else{
+        if(ISKEYDOWN(key)) is_print = 1;
+      }
     }
-    int base = uptime() + 1000;
-    if (key == _KEY_ESCAPE) {
+    //int base = uptime() + 1000;
+    if (is_halt == 1) {
       _halt(0);
     }
-    clear_screen();
-    while(uptime() < base);
+    //clear_screen();
+    draw_rect(orange,0,0,20,20);
+    //while(uptime() < base);
 
-    //next_frame += 1000 / FPS; // 计算下一帧的时间
+    next_frame += 1000 / FPS; // 计算下一帧的时间
   }
   return 0;
 }
