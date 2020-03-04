8305cb4a3554f3cbad224dadbeafd07b178edc61
tracer-nju <tracer@nju.edu.cn>
2020-03-04 17:09:06 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 12b19963635c4a6b8fe60f15a7597a35     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-88-generic       Architecture: x86-64  17:09:06 up  8:09,  1 user,  load average: 0.84, 0.29, 0.09

--- libco/co.c
+++ libco/co.c
@@ -1,15 +1,129 @@
+/**
+ * @author Bright Hong
+ * @date 2020.3.3
+ * @brief This is a shared library for coroutine.
+ * @remarks 
+ */
+
 #include "co.h"
 #include <stdlib.h>
+#include <assert.h>
+#include <setjmp.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
 
-struct co {
+
+#define DEBUG true
+#define SZ_STACK 4096
+#define NR_CO 16
+
+enum co_status {
+  CO_NEW = 1, // 新创建，还未执行过
+  CO_RUNNING, // 已经执行过
+  CO_WAITING, // 在 co_wait 上等待
+  CO_DEAD,    // 已经结束，但还未释放资源
 };
 
-struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-  return NULL;
-}
+#if defined (__i386__)
+  #define SP "%%esp"
+#elif defined (__x86_64__)
+  #define SP "%%rsp"
+#endif
 
-void co_wait(struct co *co) {
+#define stackEX(newsp, backup) \
+  asm volatile("mov " SP ", %0; mov %1, " SP : "=g"(backup) : "g"(newsp))
+#define getSP(sp) \
+  asm volatile("mov " SP ", %0" : "=g"(sp) : )
+
+struct co {
+  //int pid;
+  int state;
+  char name[32];
+  void (*func)(void *);
+  void* arg;
+  struct co* next;
+  jmp_buf buf;
+  char stack[SZ_STACK];
+  void* stack_ptr;
+};
+
+static void* stack_backup;
+static jmp_buf start_buf;
+static jmp_buf wait_buf;
+struct co* head = NULL;
+struct co* current = NULL;
+
+static struct co* co_create(const char *name, void (*func)(void *), void *arg);
+//static void co_free(struct co* co);
+
+struct co* co_start(const char *name, void (*func)(void *), void *arg) {
+  if(DEBUG) printf("Start co %s\n", name);
+  current = co_create(name, func, arg);
+  if(!setjmp(start_buf)) {
+    stackEX(current->stack_ptr, stack_backup);
+    current->func(current->arg);
+    longjmp(wait_buf, 1);
+
+  } else {
+    if(DEBUG) printf("init finished\n");
+  }
+  return current;
 }
 
 void co_yield() {
+  if(!setjmp(current->buf)) {
+    if(current->state == CO_NEW) {
+      stackEX(stack_backup, current->stack_ptr);
+      current->state = CO_WAITING;
+      longjmp(start_buf, 1);
+
+    } else {
+      struct co* next = current->next ? current->next : head;
+      current->state = CO_WAITING;
+      stackEX(next->stack_ptr, current->stack_ptr);
+      current = next;
+      longjmp(current->buf, 1);
+    }
+  } else {
+    stackEX(current->stack_ptr, stack_backup);
+    current->state = CO_RUNNING;
+    if(DEBUG) printf("go to thread %s \n",current->name);
+  }
 }
+
+void co_wait(struct co* co) {
+  if(DEBUG) printf("wait for %s \n", co->name);
+  while(co->state != CO_RUNNING) {
+    if(!setjmp(wait_buf)) {
+      current = co;
+      longjmp(co->buf, 1);
+    }
+    if(current == co) break;
+  }
+  co->state = CO_DEAD;
+  //co_free(co);
+  return;
+}
+
+static struct co* co_create(const char *name, void (*func)(void *), void *arg) {
+  struct co* ret = (struct co*) malloc(sizeof(struct co));
+  ret->state = CO_NEW;
+  strncpy(ret->name, name, sizeof(ret->name));
+  ret->func = func;
+  ret->arg = arg;
+  ret->next = NULL;
+  ret->stack_ptr = (void*)((((intptr_t)ret->stack + sizeof(ret->stack))>>4)<<4);
+  if(head) {
+    struct co* cp = head;
+    while (cp->next) {
+      cp = cp->next;
+    }
+    cp->next = ret;
+  } else {
+    head = ret;
+  }
+  return ret;
+}
+
