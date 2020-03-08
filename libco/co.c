bf86407aaecd988196de6688b41c2cd96a136bb6
tracer-nju <tracer@nju.edu.cn>
2020-03-08 20:31:43 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 12b19963635c4a6b8fe60f15a7597a35     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-88-generic       Architecture: x86-64  20:31:43 up 14:24,  1 user,  load average: 0.41, 0.17, 0.11

--- libco/co.c
+++ libco/co.c
@@ -14,11 +14,6 @@
 #include <string.h>
 #include <stdbool.h>
 
-
-#define DEBUG true
-#define SZ_STACK 4096
-#define NR_CO 16
-
 enum co_status {
   CO_NEW = 1, // 新创建，还未执行过
   CO_RUNNING, // 已经执行过
@@ -32,117 +27,91 @@ enum co_status {
   #define SP "%%rsp"
 #endif
 
-#define stackEX(newsp, backup) \
-  asm volatile("mov " SP ", %0; mov %1, " SP : "=g"(backup) : "g"(newsp))
+#define setSP(sp) \
+  asm volatile("mov " "%0," SP : : "g"(sp))
 #define getSP(sp) \
   asm volatile("mov " SP ", %0" : "=g"(sp) : )
+#define movto(dest, stack) \
+  *(uintptr_t*)(stack + (((void*)&dest)-stack_backup))=(uintptr_t)dest;
+#define MAX_CO 128
+#define DEBUG true
+#define SZ_STACK 4096
 
 struct co {
-  int state;
-  char name[32];
-  void (*func)(void *);
-  void* arg;
-  struct co* next;
-  jmp_buf buf;
-  char stack[SZ_STACK];
-  void* stack_ptr;
-};
+  union {
+    struct {
+      jmp_buf tar_buf;
+      uint8_t stat;
+    };
+    uint8_t stack[SZ_STACK];
+  };
+}routines[MAX_CO],*current;
 
-static void* stack_backup;
-static jmp_buf start_buf;
-static jmp_buf wait_buf;
-struct co* head = NULL;
-struct co* current = NULL;
+static int pool[MAX_CO];
+static int pool_id = 0;
+static jmp_buf ret_buf;
+void* stack_backup = NULL;
 
-static struct co* co_create(const char *name, void (*func)(void *), void *arg);
-static void co_free(struct co* co);
+static struct co* new_co() {
+  pool_id--;
+  routines[pool[pool_id]].stat = CO_NEW;
+  return &routines[pool[pool_id]];
+}
+
+void co_init() {
+  int i;
+  pool_id = MAX_CO;
+  for(i=0; i<MAX_CO; i++){
+    pool[i] = i;
+    routines[i].stat = 0;
+  }
+}
 
 struct co* co_start(const char *name, void (*func)(void *), void *arg) {
-  if(DEBUG) printf("Start co %s\n", name);
-  current = co_create(name, func, arg);
-  if(DEBUG) printf("created but no context switch\n");
-  if(!setjmp(start_buf)) {
-    stackEX(current->stack_ptr, stack_backup);
-    current->func(current->arg);
-    longjmp(wait_buf, 1);
-
-  } else {
-    if(DEBUG) printf("init finished\n");
+  if(pool_id==0){
+    co_init();
   }
-  return current;
+  getSP(stack_backup);
+  current = new_co();
+  uint8_t* stack_top = current->stack + SZ_STACK - SZ_STACK/4;
+  movto(name,stack_top);
+  movto(func,stack_top);
+  movto(arg,stack_top);
+
+  setSP(stack_top);
+  if(!setjmp(current->tar_buf)) {
+    setSP(stack_backup);
+    return current;
+  } else {
+    //current->stat = CO_RUNNING;
+    func(arg);
+    current->stat = CO_DEAD;
+    pool[pool_id++] = current-routines;
+    longjmp(ret_buf,1);
+  }
+  return NULL;
+}
+
+static void co_jmp(struct co* co){
+  current = co;
+  longjmp(co->tar_buf,1);
 }
 
 void co_yield() {
-  if(!setjmp(current->buf)) {
-    if(current->state == CO_NEW) {
-      stackEX(stack_backup, current->stack_ptr);
-      current->state = CO_WAITING;
-      longjmp(start_buf, 1);
-
-    } else {
-      struct co* next = current->next ? current->next : head;
-      current->state = CO_WAITING;
-      stackEX(next->stack_ptr, current->stack_ptr);
-      current = next;
-      longjmp(current->buf, 1);
-    }
-  } else {
-    stackEX(current->stack_ptr, stack_backup);
-    current->state = CO_RUNNING;
-    if(DEBUG) printf("go to thread %s \n",current->name);
+  if(!setjmp(current->tar_buf)){
+    int next_co;
+    do{
+      next_co = rand()%MAX_CO;
+    } while(!(routines[next_co].stat==CO_NEW));
+    co_jmp(routines+next_co);
   }
 }
 
 void co_wait(struct co* co) {
-  if(DEBUG) printf("wait for %s \n", co->name);
-  while(co->state != CO_RUNNING) {
-    if(!setjmp(wait_buf)) {
-      current = co;
-      longjmp(co->buf, 1);
-    }
-    if(current == co) break;
-  }
-  co->state = CO_DEAD;
-  co_free(co);
-  return;
-}
-
-static struct co* co_create(const char *name, void (*func)(void *), void *arg) {
-  struct co* ret = (struct co*) malloc(sizeof(struct co));
-  ret->state = CO_NEW;
-  strncpy(ret->name, name, sizeof(ret->name));
-  ret->func = func;
-  ret->arg = arg;
-  ret->next = NULL;
-  ret->stack_ptr = (void*)((((intptr_t)ret->stack + sizeof(ret->stack))>>4)<<4);
-  if(head) {
-    struct co* cp = head;
-    while (cp->next) {
-      cp = cp->next;
-    }
-    cp->next = ret;
-  } else {
-    head = ret;
-  }
-  return ret;
-}
-
-static void co_free(struct co* co) {
-  if(!head) return;
-  if(head == co) {
-    struct co* next = head->next;
-    free(head);
-    head = next;
-  }
-  if(head) {
-    struct co* cp = head;
-    while (cp->next) {
-      if(cp->next == co){
-        struct co* next = cp->next->next;
-        free(cp->next);
-        break;
-      }
-      cp = cp->next;
+  if(!setjmp(ret_buf)) {
+    while(co->stat == CO_NEW){
+      co_jmp(co);
     }
   }
+  co->stat = 0;
 }
