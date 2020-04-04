c83ca4f016f3fb735ee4db1aec86a4c0fcf78abc
tracer-nju <tracer@nju.edu.cn>
2020-04-04 22:16:44 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 72cc9ad53f954d59bafaf8f38199c645     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-91-generic       Architecture: x86-64  22:16:44 up 17 min,  1 user,  load average: 0.10, 0.18, 0.17

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -10,23 +10,20 @@ static void os_run() {
     _putc(*s == '*' ? '0' + _cpu() : *s);
   }
   */
-  printf("os test\n");
+  //printf("os test\n");
   //assert(pmm->alloc(4096)!=NULL);
   printf("first test\n");
   intptr_t* sg = 0;
+  /*
   for(int i=0; i< 10; i++){
     sg = pmm->alloc(10);
     pmm->free(sg);
-    /*
-    if(sg==NULL) {
-      continue;
-    } else {
-      assert((((intptr_t)sg)&(1<<12))==((intptr_t)sg));
-    }
-    */
   }
-  printf("succeed!\n");
-  while (1) ;
+  */
+  sg = pmm->alloc(10);
+  printf("ptr %ld\n",sg);
+  //printf("succeed!\n");
+  //while (1) ;
 }
 
 MODULE_DEF(os) = {
