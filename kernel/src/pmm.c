62ee71d4be08e7c7748c44f21d04f9d78631c78c
tracer-nju <tracer@nju.edu.cn>
2020-04-05 17:22:49 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 2d404805ba3940b193e5110988b44281     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-91-generic       Architecture: x86-64  17:22:49 up 8 min,  1 user,  load average: 0.02, 0.19, 0.15

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -90,7 +90,7 @@ static void* alloc_small(size_t size) {
   cur_page->chart->next = (mem_head*)align(((uintptr_t)tmp+tmp->size), getb(size));
   cur_page->chart->next->size = size;
   cur_page->chart->hd_sp = (intptr_t)cur_page;
-  printf("page head %ld\n",(intptr_t)cur_page);
+  printf("page head %ld\n",cur_page->chart->hd_sp);
   //cur_page->chart->next->prev = cur_page->chart;
   //mutex_unlock(&private_list[cpu_id]->lock);
   cur_page->count += 1;
