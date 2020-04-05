e4e9efef4905dd32831b3d8fd4c4d7c62d1770a2
tracer-nju <tracer@nju.edu.cn>
2020-04-05 16:43:02 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 8e32346e4e76437ca0f9ecfc1ceae5e0     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-91-generic       Architecture: x86-64  16:43:03 up  2:33,  1 user,  load average: 0.68, 0.56, 0.29

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -102,6 +102,7 @@ static void *kalloc(size_t size) {
     page_t* cur = (page_t*)private_list[cpu_id];
     //size_t rem = ((uintptr_t)cur)+PAGE_SIZE-((uintptr_t)cur->chart+cur->chart->size);
     size_t used = align((intptr_t)(cur->chart),getb(size))+size+SG_SIZE-(intptr_t)cur;
+    printf("decide page alloc\n");
     if(used > PAGE_SIZE) {
       mutex_lock(&big_lock);
       page_t* tmp = alloc_new_page();
@@ -118,7 +119,7 @@ static void *kalloc(size_t size) {
         private_list[cpu_id]->count = 0;
       }
     }
-    printf("begin small alloc \n");
+    //printf("begin small alloc \n");
     return alloc_small(size);
   }
   return NULL;
