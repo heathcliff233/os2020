4b9027b74cb0203ad3265ef926000d2e3dcfb9a1
tracer-nju <tracer@nju.edu.cn>
2020-04-05 21:51:23 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 12e1935759f44289bfb97ebc80876866     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-91-generic       Architecture: x86-64  21:51:23 up 1 min,  1 user,  load average: 0.87, 0.57, 0.23

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -24,6 +24,8 @@ static void os_run() {
     if(sg==NULL) count++;
     sg = pmm->alloc(132);
     if(sg==NULL) count++;
+    sg = pmm->alloc(532);
+    if(sg==NULL) count++;
     //printf("NO %d alloc succ\n",i);
     //if (((intptr_t)sg)/4096*4096!=(intptr_t)sg) printf("NO %d ptr %ld",i, (intptr_t)sg);
     //printf("to free\n");
