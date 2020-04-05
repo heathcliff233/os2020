3912e716451e1e6065def7f99ed2e72a84c41adf
tracer-nju <tracer@nju.edu.cn>
2020-04-05 16:47:09 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 8e32346e4e76437ca0f9ecfc1ceae5e0     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-91-generic       Architecture: x86-64  16:47:09 up  2:37,  1 user,  load average: 0.14, 0.37, 0.27

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -102,7 +102,9 @@ static void *kalloc(size_t size) {
     page_t* cur = (page_t*)private_list[cpu_id];
     //size_t rem = ((uintptr_t)cur)+PAGE_SIZE-((uintptr_t)cur->chart+cur->chart->size);
     size_t used = align((intptr_t)(cur->chart),getb(size))+size+SG_SIZE-(intptr_t)cur;
-    printf("used %ld\n",used);
+    //printf("used %ld\n",used);
+    printf("page ptr %ld",(intptr_t)cur);
+    printf("memblock ptr %ld",(intptr_t)(cur->chart));
     if(used > PAGE_SIZE) {
       mutex_lock(&big_lock);
       page_t* tmp = alloc_new_page();
