20180d37a9e5ff484df50b428fabc380f524ebd0
tracer-nju <tracer@nju.edu.cn>
2020-04-06 11:06:36 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: d4b6f98279eb4d92af579a51dec043ac     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-91-generic       Architecture: x86-64  11:06:36 up 6 min,  1 user,  load average: 6.48, 3.85, 1.69

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -68,7 +68,7 @@ static void os_run() {
     if(sg==NULL) count++;
     */
     
-    printf("NO %d loop succ\n",i);
+    printf("cpu %d NO %d loop succ\n",_cpu(),i);
     printf("miss %d\n",count);
     //if (((intptr_t)sg)/4096*4096!=(intptr_t)sg) printf("NO %d ptr %ld",i, (intptr_t)sg);
     //printf("to free\n");
