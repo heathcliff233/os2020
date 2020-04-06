cbd03945dfa3df68b694eaaf204e7a7a05c92e51
tracer-nju <tracer@nju.edu.cn>
2020-04-06 13:56:02 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: d4b6f98279eb4d92af579a51dec043ac     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-91-generic       Architecture: x86-64  13:56:02 up  2:55,  1 user,  load average: 15.29, 15.16, 15.06

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -28,7 +28,7 @@ static void os_run() {
   //tst = malloc(10*sizeof(intptr_t));
   int j=0;
   int k=0;
-  for(int i=0; i<1000; i++){
+  for(int i=0; i<1; i++){
     
     for(j=0; j<1000; j++){
       tst[j] = NULL;
