63ab7c6d9cd0e188089464245dbff45ba12e400b
tracer-nju <tracer@nju.edu.cn>
2020-04-08 09:29:43 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 3a200ab9c0164b26a2057e7d85d1eb20     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  09:29:43 up 33 min,  1 user,  load average: 0.26, 0.51, 0.41

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -82,7 +82,7 @@ static bool full(page_t* page){
 }
 
 static void* kalloc(size_t size) {
-  //if(DEBUG)printf("begin alloc\n");
+  if(DEBUG)printf("begin alloc\n");
   assert(size > 0);
   assert(size <= 4096);
   int bits = getb(size);
@@ -113,12 +113,12 @@ static void* kalloc(size_t size) {
     j--;
   }
   assert(j>=0);
-  if(DEBUG)printf("change bitmap\n");
+  //if(DEBUG)printf("change bitmap\n");
   mutex_lock(&big_lock);
   cur->bitmap[i] |= (1<<j);
   cur->count += 1;
   mutex_unlock(&big_lock);
-  if(DEBUG)printf("finish kalloc\n");
+  //if(DEBUG)printf("finish kalloc\n");
   return (void*)(((uintptr_t)cur)+((i*32+j)<<bits));
 
 }
