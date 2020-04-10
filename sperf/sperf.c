ff140b8da3c67d4192f19e5ae155269629accd4d
tracer-nju <tracer@nju.edu.cn>
2020-04-10 17:10:04 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: ff162328ee434e0d9c1260f28da80313     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  17:10:04 up 12:37,  1 user,  load average: 4.31, 4.14, 4.06

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -51,9 +51,9 @@ void child_proc(int fd, int argc, char* argv[], char* envp[]){
 		strace_args[i+1] = argv[i];
 	}
 
-	char avai_path[512] = {0};
-	strcpy(avai_path, getenv("PATH"));
-	char full_path[100] = {0};
+	char* avai_path;
+	avai_path = getenv("PATH");
+	char full_path[100];
 	char* tok_piece = strtok(avai_path, ":");
 	strcpy(full_path, tok_piece);
 	strcat(full_path, "/strace");
@@ -61,6 +61,7 @@ void child_proc(int fd, int argc, char* argv[], char* envp[]){
 	while((execve(tok_piece, strace_args, envp))==-1){
 		tok_piece = strtok(NULL, ":");
 		//assert(0);
+		memset(full_path, '\0', 100);
 		strcpy(full_path, tok_piece);
 		strcat(full_path, "/strace");
 		printf("full path%s", full_path);
