6fae426303565b23742a327c1ca8523c046b6486
tracer-nju <tracer@nju.edu.cn>
2020-04-10 15:57:04 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: ff162328ee434e0d9c1260f28da80313     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  15:57:04 up 11:24,  1 user,  load average: 4.33, 4.25, 4.32

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,13 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-int main(int argc, char *argv[]) {
-  char *exec_argv[] = { "strace", "ls", NULL, };
-  char *exec_envp[] = { "PATH=/bin", NULL, };
-  execve("strace",          exec_argv, exec_envp);
-  execve("/bin/strace",     exec_argv, exec_envp);
-  execve("/usr/bin/strace", exec_argv, exec_envp);
-  perror(argv[0]);
-  exit(EXIT_FAILURE);
+#include <string.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/*
+int main(int argc, char* argv[]){
+ 	char *exec_argv[] = { "strace", "ls", NULL, };
+ 	char *exec_envp[] = { "PATH=/bin", NULL, };
+ 	execve("strace",          exec_argv, exec_envp);
+ 	execve("/bin/strace",     exec_argv, exec_envp);
+ 	execve("/usr/bin/strace", exec_argv, exec_envp);
+ 	perror(argv[0]);
+ 	exit(EXIT_FAILURE);
 }
+*/
+void child_proc(int fd, int argc, char* argv[], char* envp[]);
+void parent_proc(int fd);
+
+int main(int argc, char* argv[], char* envp[]) {
+ 	int pid_cp = -1;
+ 	int pipefd[2] = {};
+ 	pid_cp = fork();
+ 	assert(pipe(pipefd) != -1);
+ 	assert(pid_cp != -1);
+
+ 	if(pid_cp == 0){
+ 		close(pipefd[0]);
+ 		child_proc(pipefd[1], argc, argv, envp);
+ 		assert(0);
+
+ 	} else {
+ 		close(pipefd[1]);
+ 		parent_proc(pipefd[0]);
+ 		close(pipefd[0]);
+ 	}
+}
+
+void child_proc(int fd, int argc, char* argv[], char* envp[]){
+	int ig = open("/dev/null", 0);
+	dup2(ig, 1);
+	dup2(ig, 2);
+
+	char** strace_args = malloc(sizeof(char*)*(argc+2));
+	strace_args[0] = "strace";
+	strace_args[1] = "-T";
+	for(int i=1; i<=argc; i++){
+		strace_args[i+1] = argv[i];
+	}
+
+	char avai_path[512] = {0};
+	strcpy(avai_path, getenv("PATH"));
+	char full_path[100] = {0};
+	char* tok_piece = strtok(avai_path, ":");
+	strcpy(full_path, tok_piece);
+	strcat(full_path, "/strace");
+	printf("path %s\n", full_path);
+	while((execve(tok_piece, strace_args, envp))==-1){
+		tok_piece = strtok(NULL, ":");
+		strcpy(full_path, tok_piece);
+		strcat(full_path, "/strace");
+		printf("full path%s", full_path);
+	}
+	assert(0);
+}
+
+void parent_proc(int fd){
+}
+
+
+
+
+
+
