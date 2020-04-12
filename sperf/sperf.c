3d041dcef27796137fd7192538e50357636838f7
tracer-nju <tracer@nju.edu.cn>
2020-04-12 11:24:04 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 0d55a8f8c7c242ed8af99b2859c9e037     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  11:24:04 up  2:10,  1 user,  load average: 0.01, 0.03, 0.00

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -151,7 +151,7 @@ void parent_proc(int fd){
 			fflush(stdout);
 		}
 	}
-	for(int j=0; j<5; j++){
+	for(int j=0; j<i; j++){
 		printf("%s time %d\n",call_list[i].name,(int)(call_list[i].time));
 	}
 	for(int k=0; k<80; k++){
