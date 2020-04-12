e757467a84c9db2282427573869ffd0805520e84
tracer-nju <tracer@nju.edu.cn>
2020-04-12 11:09:59 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 0d55a8f8c7c242ed8af99b2859c9e037     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  11:09:59 up  1:56,  1 user,  load average: 0.25, 0.08, 0.02

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -66,7 +66,7 @@ void child_proc(int* fd, int argc, char* argv[], char* envp[]){
 	strcat(full_path, "/strace");
 	printf("path %s\n", full_path);
 	
-	while((execve(tok_piece, strace_args, envp))==-1){
+	while((execve(full_path, strace_args, envp))==-1){
 		//perror("shit not this");
 		//tok_piece = strtok(NULL, ":");
 		//assert(0);
@@ -79,7 +79,7 @@ void child_proc(int* fd, int argc, char* argv[], char* envp[]){
 	}
 	assert(0);
 }
-
+/*
 static int readl(int fd, char* line){
 	char ch;
 	int ptr = 0;
@@ -107,9 +107,9 @@ typedef struct syscallStruct{
 int compare_list(const void* p1, const void* p2){
 	return ((*(sys_t *)p1).time > (*(sys_t *)p2).time)?-1:1;
 }
-
+*/
 void parent_proc(int fd){
-	/*	
+	
 	char ch;
 	sleep(5);
 	read(fd, &ch, 1);
@@ -118,7 +118,8 @@ void parent_proc(int fd){
 	}
 	printf("\n");
 	//while(1);
-	*/
+	
+	/*
 	printf("in father proc\n");	
 	char line[1024] = "";
 	int wstatus = 0;
@@ -148,6 +149,7 @@ void parent_proc(int fd){
 			fflush(stdout);
 		}
 	}
+	*/
 	
 	
 }
