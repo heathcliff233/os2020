45b9c96ebfb29a3be8babbd3d02bba6807ff091f
tracer-nju <tracer@nju.edu.cn>
2020-04-12 10:51:57 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 0d55a8f8c7c242ed8af99b2859c9e037     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  10:51:57 up  1:38,  1 user,  load average: 0.04, 0.03, 0.00

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -121,7 +121,7 @@ void parent_proc(int fd){
 	sys_t call_list[1000];
 
 	int i = 0;
-	while(waitpid(-1, &wstatus, WNOHANG) == 0 && readl(fd, line) >= 0){
+	while(waitpid(-1, &wstatus, WNOHANG) == 0){// && readl(fd, line) >= 0){
 		printf("fuck it \n");
 		if(call_list[i].name[0] == 0){
 			sscanf(line, "%[^(]%*[^<]<%lf>", call_list[i].name, &(call_list[i].time));
