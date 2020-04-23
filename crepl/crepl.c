a9b7c59901e473da93ed6342776ce4a36f77b651
tracer-nju <tracer@nju.edu.cn>
2020-04-23 21:06:52 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 79d79df5ad6141ae804547bdefeb52f0     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  21:06:52 up  3:10,  1 user,  load average: 0.34, 0.17, 0.09

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
     	printf("get one\n");
     }
     
-    char tmp_file[32] = "/tmp/XXXXXX";
+    char tmp_file[32] = "./tmp/XXXXXX";
     //sprintf(src, "./%s", tmp_file);
     int fd = mkstemp(tmp_file);
     if(fd<0)printf("fail to create tmp\n");
@@ -52,17 +52,15 @@ int main(int argc, char *argv[]) {
 
     strcpy(src, tmp_file);
     //sprintf(src, "/proc/self/fd/%d", fd);
-    sprintf(out, "./%s.so", src);
+    sprintf(out, "%s.so", src);
 
 	//printf("%s\n", src);
 	int ppid = fork();
 	if(ppid == 0){
 		//close(1);
 		//close(2);
-		char* shit[] = {"cat", src};
-		execvp("cat", shit);
-		//execvp("gcc",cargv);
-		//assert(0);
+		execvp("gcc",cargv);
+		assert(0);
 	} //else {
 
 		//unlink(tmp_file);
