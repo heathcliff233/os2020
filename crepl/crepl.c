7d98b3948a7ff071787894993adc77d2c5f1d36f
tracer-nju <tracer@nju.edu.cn>
2020-04-23 21:09:00 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 79d79df5ad6141ae804547bdefeb52f0     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  21:09:00 up  3:12,  1 user,  load average: 0.05, 0.12, 0.08

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 
 //void compile(char* path);
-char src[32], out[32];
+char src[128], out[128];
 #if defined(__i386__)
 	#define CC_ABI "-m32"
 #elif defined(__x86_64__)
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
     	printf("get one\n");
     }
     
-    char tmp_file[32] = "./tmp/XXXXXX";
+    char tmp_file[128] = "/media/psf/Home/Desktop/daerxia/os20/os-workbench/crepl/tmp/XXXXXX";
     //sprintf(src, "./%s", tmp_file);
     int fd = mkstemp(tmp_file);
     if(fd<0)printf("fail to create tmp\n");
