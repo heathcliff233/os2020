9331da446676e9803636e34a997960fc5ddcd22f
tracer-nju <tracer@nju.edu.cn>
2020-04-23 20:54:04 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 79d79df5ad6141ae804547bdefeb52f0     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  20:54:04 up  2:57,  1 user,  load average: 0.16, 0.08, 0.06

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
     	printf("get one\n");
     }
     
-    char tmp_file[32] = "XXXXXX";
+    char tmp_file[32] = "./tmp/XXXXXX";
     //sprintf(src, "./%s", tmp_file);
     int fd = mkstemp(tmp_file);
     if(fd<0)printf("fail to create tmp\n");
@@ -50,8 +50,8 @@ int main(int argc, char *argv[]) {
 
     //========compile=======
 
-    //strcpy(src, tmp_file);
-    sprintf(src, "/proc/self/fd/%d", fd);
+    strcpy(src, tmp_file);
+    //sprintf(src, "/proc/self/fd/%d", fd);
     sprintf(out, "./%s.so", src);
 
 	//printf("%s\n", src);
