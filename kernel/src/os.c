8b444079a0f980222dcacf65b0fc13198bb267e9
tracer-nju <tracer@nju.edu.cn>
2020-05-01 15:04:07 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 79d79df5ad6141ae804547bdefeb52f0     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  15:04:07 up  6:19,  1 user,  load average: 0.16, 0.15, 0.17

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -13,6 +13,15 @@ static irq_handler_t root_handler = {
 static void os_init() {
   pmm->init();
   kmt->init();
+
+#ifdef DEBUG
+  kmt->sem_init(&empty, "empty", 5);  // 缓冲区大小为 5
+  kmt->sem_init(&fill,  "fill",  0);
+  for (int i = 0; i < 4; i++) // 4 个生产者
+    kmt->create(task_alloc(), "producer", producer, NULL);
+  for (int i = 0; i < 5; i++) // 5 个消费者
+    kmt->create(task_alloc(), "consumer", consumer, NULL);
+#endif
 }
 
 static void os_run() {
