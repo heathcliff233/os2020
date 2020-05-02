82a4c7cdcbd05a0355f56d787f0472b38685c02e
tracer-nju <tracer@nju.edu.cn>
2020-05-02 16:05:39 +0800

    Static hostname: apple          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: dbbf2e64ee1647589d51101051352eda     Virtualization: kvm   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-99-generic       Architecture: x86-64  16:05:39 up  5:05,  1 user,  load average: 0.09, 0.10, 0.07

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -3,22 +3,41 @@
 typedef struct irq{
     int seq,event;
     handler_t handler;
-    struct irq *next;
+    int valid;
+    //struct irq *next;
 } irq_handler_t;
-
+/*
 static irq_handler_t root_handler = {
   0, _EVENT_NULL, NULL, NULL
 };
-
+*/
 
 void echo_test(void* arg) {
   while(1) putstr("ass");
 }
-
-
+//===========================================================
+#define INT_SEQ_MIN 0
+#define INT_SEQ_MAX 4
+#define INT_NR_MAX 4
+static irq_handler_t trap_handlers[INT_SEQ_MAX][INT_NR_MAX];
+static _Context* trap_handler_invalid(_Event e, _Context* c) {
+    // should not reach here
+    assert(0);
+}
+static void trap_init() {
+    for (int i = INT_SEQ_MIN; i < INT_SEQ_MAX; i++) {
+        for (int j = 0; j < INT_NR_MAX; j++) {
+            trap_handlers[i][j].handler = trap_handler_invalid;
+            trap_handlers[i][j].valid = 0;
+        }
+    }
+}
+//============================================================
 static void os_init() {
   pmm->init();
   kmt->init();
+
+  trap_init();
 
 //#ifdef DEBUG
 /*
@@ -38,17 +57,14 @@ static void os_init() {
 
 static void os_run() {
   _intr_write(1);
-  //putstr("ass\n");
-  while(1){
-    //_yield();
-  }
+  while(1);
   panic("shit failed!\n");
 }
 
+/*
 static _Context *os_trap(_Event ev, _Context *ctx) {
   _Context *next = NULL;
   for(irq_handler_t* h = (&root_handler)->next; h!=NULL; h=h->next) {
-  //for (auto &h: handlers_sorted_by_seq) {
     if (h->event == _EVENT_NULL || h->event == ev.event) {
       _Context *r = h->handler(ev, ctx);
       panic_on(r && next, "returning multiple contexts");
@@ -56,7 +72,6 @@ static _Context *os_trap(_Event ev, _Context *ctx) {
     }
   }
   panic_on(!next, "returning NULL context");
-  //panic_on(sane_context(next), "returning to invalid context");
   return next;
 }
 
@@ -69,8 +84,7 @@ static void os_on_irq(int seq, int event, handler_t handler) {
         p=p->next;
     }
     
-    prev->next = pmm->alloc(sizeof(handler_t));//new(root_handler);
-    //putstr("on_irq\n");
+    prev->next = pmm->alloc(sizeof(handler_t));
     prev=prev->next;
     
     prev->seq=seq;
@@ -78,6 +92,57 @@ static void os_on_irq(int seq, int event, handler_t handler) {
     prev->handler=handler;
     prev->next=p;
 }
+*/
+
+//==============================================================
+static _Context* os_trap(_Event ev, _Context* context) {
+    // util_log("TRAP", trap_nr[_cpu()]++, LOG_WARNING, LOG_NHEX);
+    _Context* ret = NULL;
+    for (int i = INT_SEQ_MIN; i < INT_SEQ_MAX; i++) {
+        for (int j = 0; j < INT_NR_MAX; j++) {
+            if (trap_handlers[i][j].valid) {
+                if (trap_handlers[i][j].event == ev.event ||
+                    trap_handlers[i][j].event == _EVENT_NULL) {
+                    _Context* next = trap_handlers[i][j].handler(ev, context);
+                    // util_log("Trap succ", (uint32_t)next, LOG_SUCCESS,
+                    //          LOG_NHEX);
+                    if (next != NULL) {
+                        ret = next;
+                    }
+                }
+            } else {
+                break;
+            }
+        }
+    }
+    if (ret == NULL) {
+        ret = context;
+    }
+    return ret;
+}
+
+static void os_on_irq(int seq, int event, handler_t handler) {
+    int ptr;
+    for (ptr = INT_SEQ_MIN; ptr < INT_NR_MAX; ptr++) {
+        if (trap_handlers[seq][ptr].valid == 0) {
+            break;
+        }
+    }
+    if (ptr == INT_NR_MAX) {
+        // No free slot, should enlarge INT_NR_MAX
+        assert(0);
+        return;
+    }
+    trap_handlers[seq][ptr].valid = 1;
+    trap_handlers[seq][ptr].seq = seq;
+    trap_handlers[seq][ptr].event = event;
+    trap_handlers[seq][ptr].handler = handler;
+    printf("finish on_irq\n");
+    // util_log("Trap init", trap_handlers[seq][ptr].valid, LOG_SUCCESS,
+    // LOG_NHEX); util_log("Trap init", seq, LOG_SUCCESS, LOG_NHEX);
+    // util_log("Trap init", ptr, LOG_SUCCESS, LOG_NHEX);
+}
+//==============================================================
 
 MODULE_DEF(os) = {
   .init = os_init,
